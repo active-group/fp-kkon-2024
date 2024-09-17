450bfaaeedf32e737399e127be8ee064e350d592
Marco Schneider <marco.schneider@active-group.de>
2024-09-17 09:09:02 +0200

    initial commit

---
+++ src/main/kotlin/Animal.kt
@@ -0,0 +1,17 @@
+data class Dillo (
+    val weight: Int,
+    val isAlive: Boolean
+)
+
+// Ein Dillo mit 500g Futter füttern
+fun feedDillo(dillo: Dillo): Dillo {
+    Dillo()
+    dillo.weight
+    dillo.isAlive
+}
+
+fun runOverDillo(dillo: Dillo): Dillo {
+    Dillo()
+    dillo.weight
+    dillo.isAlive
+}

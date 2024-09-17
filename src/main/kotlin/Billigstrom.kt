f1d2a8faa2ba0c75f604bfafedf80398a853defd
Marco Schneider <marco.schneider@active-group.de>
2024-09-17 09:13:04 +0200

    remove spoilers

--- src/main/kotlin/Billigstrom.kt
+++
@@ -1,6 +0,0 @@
-/**
- * | Tarif              | Grundgebühr pro Monat | Verbrauchspreis pro kWh |
- * |--------------------+-----------------------+-------------------------|
- * | "Billig-Strom"     | € 4.90                | € 0.19                  |
- * | "Watt fuer Wenig"  | € 8.20                | € 0.16                  |
- */
